98b5b70f6b049474a67a2bc1d46378ec066ff0a3
Pranshu Tripathi <pranshu.tripathi@schrodinger.com>
2024-10-20 18:36:39 +0530

    Added JWT token and cookie session in auth service.

--- authentication/src/models/user.ts
+++ authentication/src/models/user.ts
@@ -5,47 +5,57 @@ import { Password } from "../services/password";
 // that are required to create a new User
 
 interface UserAttributes {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 // An interface that describes the properties
 // that a User Model has
 interface UserModel extends mongoose.Model<UserDocument> {
-    build(attributes: UserAttributes): UserDocument;
-};
+  build(attributes: UserAttributes): UserDocument;
+}
 
 // An interface that describes the properties
 // that a User Document has
 interface UserDocument extends mongoose.Document {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
-    }
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+      },
+      versionKey: false,
+    },
+  }
+);
 
-});
-
-userSchema.pre("save", async function(done) {
-    if(this.isModified("password")) {
-        const hashed = await Password.toHash(this.get("password"));
-        this.set("password", hashed);
-    }
-    done();
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
 });
 
 userSchema.statics.build = (attributes: UserAttributes) => {
-    return new User(attributes);
-}
+  return new User(attributes);
+};
 
 const User = mongoose.model<UserDocument, UserModel>("User", userSchema);
 
