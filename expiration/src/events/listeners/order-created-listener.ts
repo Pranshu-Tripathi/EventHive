c1b9a948789d5229d70aa0cbed2d7d849144ca81
Pranshu Tripathi <pranshu.tripathi@schrodinger.com>
2024-11-09 13:27:29 +0530

    014 Implemented the Expiration Service
    	- Used Bull js as a message broker.
    	- Used a redis deployment for queueing of jobs
    	- Added tests for the same

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,27 @@
+import {
+  Listener,
+  Subjects,
+  OrderCreatedEvent,
+  OrderStatus,
+} from "@eventhive/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queue/expiration-queue";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
+    msg.ack();
+  }
+}
